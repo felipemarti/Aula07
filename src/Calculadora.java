b18baec65b4a40ac0c166c034f21e77980ee385f
Felipe Pereira <felipe.mart.pereira@gmail.com>
2021-07-08 16:41:19 -0300

    Implementação final da calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,18 +2,6 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-	public static void exibirMenu() {
-		System.out.println("PROGRAMA CALCULADORA");
-		System.out.println("Escolha sua opção!");
-		System.out.println("1 - Digitar valores");
-		System.out.println("2 - Realizar soma");
-		System.out.println("3 - Realizar subtração");
-		System.out.println("4 - Realizar divisão");
-		System.out.println("5 - Realizar multiplicação");
-		System.out.println("6 - Sair");	
-	}
-	
-	
 	
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
@@ -21,7 +9,7 @@ public class Calculadora {
 		int opcao = 0;
 		
 		while (opcao!=6) {
-			exibirMenu();
+			FuncoesImpressao.exibirMenu();
 			opcao = leitor.nextInt();
 			
 			switch(opcao) {
---
+++ src/FuncoesImpressao.java
@@ -0,0 +1,14 @@
+
+public class FuncoesImpressao {
+
+	public static void exibirMenu() {
+		System.out.println("PROGRAMA CALCULADORA");
+		System.out.println("Escolha sua opção!");
+		System.out.println("1 - Digitar valores");
+		System.out.println("2 - Realizar soma");
+		System.out.println("3 - Realizar subtração");
+		System.out.println("4 - Realizar divisão");
+		System.out.println("5 - Realizar multiplicação");
+		System.out.println("6 - Sair");	
+	}
+}
